6412071c8d3dcf0bdccf99b3e866800a759b373b
Santi <santiago.rey2@udc.es>
2021-09-15 13:29:44 +0200

    Algoritmos implementados faltan tests

--- p1.c
+++ p1.c
@@ -6,15 +6,39 @@ int sumaSubMax1 (int v[],int n){
     for (i = 0; i < n-1; i++){
         estasuma = 0;
         for(j = i;j < n-1; j++){
-            estasuma = estasuma + v[j];
+            estasuma += v[j];
 
             if(estasuma > sumamax){
                 sumamax = estasuma;
-                
             }
         }
     }
 
+    return sumamax;
 
+}
+
+int sumaSubMax2 ( int v[],int n){
+    int estasuma = 0, sumamax = 0,j;
+
+    for(j = 0 ; j < n-1 ; j++){
+        estasuma += v[j];
+
+        if(estasuma > sumamax){
+            sumamax = estasuma;
+        }else if(estasuma < 0){
+            estasuma = 0;
+        }
+    }
+
+    return sumamax;
+
+}
+
+void test1(){
+    
+}
+
+int main(){
 
 }
