9c580750d81f26e6e27ee96b476c9ab24b170c45
Daniela <83793846+danicisneross@users.noreply.github.com>
2021-09-24 11:28:20 +0200

    Update p1.c

--- p1.c
+++ p1.c
@@ -5,50 +5,45 @@
 #include <sys/time.h>
 
 
-int sumaSubMax1 (int v[],int n){
-    int sumamax = 0,estasuma,j,i;
+int sumaSubMax1(int v[], int n) {
+    int sumaMax = 0, estaSuma, j, i;
 
-    for (i = 0; i < n; i++){
-        estasuma = 0;
-        for(j = i;j < n; j++){
-            estasuma += v[j];
-
-            if(estasuma > sumamax){
-                sumamax = estasuma;
+    for (i = 0; i < n; i++) {
+        estaSuma = 0;
+        for (j = i; j < n; j++) {
+            estaSuma += v[j];
+            if (estaSuma > sumaMax) {
+                sumaMax = estaSuma;
             }
         }
     }
-
-    return sumamax;
-
+    return sumaMax;
 }
 
-int sumaSubMax2 ( int v[],int n){
-    int estasuma = 0, sumamax = 0,j;
+int sumaSubMax2(int v[], int n) {
+    int estaSuma = 0, sumaMax = 0, j;
 
-    for(j = 0 ; j < n ; j++){
-        estasuma += v[j];
-
-        if(estasuma > sumamax){
-            sumamax = estasuma;
-        }else if(estasuma < 0){
-            estasuma = 0;
+    for (j = 0; j < n; j++) {
+        estaSuma += v[j];
+        if (estaSuma > sumaMax) {
+            sumaMax = estaSuma;
+        } else if (estaSuma < 0) {
+            estaSuma = 0;
         }
     }
-
-    return sumamax;
-
+    return sumaMax;
 }
 
-void listarvector(int v[],int n){
+void listarvector(int v[], int n) {
     int i;
-    for (i = 0;i < n;i++){
-        if(i == 0){
-            printf("[ %2d, ",v[i]);
-        }else if(i < n-1){
-            printf("%2d, ",v[i]);
-        }else{
-            printf("%2d] ",v[i]);
+    
+    for (i = 0; i < n; i++) {
+        if (i == 0) {
+            printf("[ %2d, ", v[i]);
+        } else if (i < n - 1) {
+            printf("%2d, ", v[i]);
+        } else {
+            printf("%2d] ", v[i]);
         }
     }
 }
@@ -70,126 +65,115 @@ void inicializarvector(int v[],int n,int i){
 }
 
 void inicializar_semilla() {
-srand(time(NULL));
+    srand(time(NULL));
 /* se establece la semilla de una nueva serie de enteros pseudo-aleatorios */
 }
 
-void aleatorio(int v [], int n) {
-int i, m=2*n+1;
-for (i=0; i < n; i++)
-v[i] = (rand() % m) - n;
+void aleatorio(int v[], int n) {
+    int i, m = 2 * n + 1;
+    
+    for (i = 0; i < n; i++)
+        v[i] = (rand() % m) - n;
 /* se generan números pseudoaleatorio entre -n y +n */
 }
 
-void test1(){
-    int n = 5;
-    int v[n],i;
+void test1() {
+    int n = 5, v[n], i;
+    
     printf("                      subM1   subM2 \n");
-    for(i=0; i < 6;i++){
-        inicializarvector(v,n,i);
-        listarvector(v,n);
-        printf(" %3d   %3d  \n",sumaSubMax1(v,n),sumaSubMax2(v,n));
+    for (i = 0; i < 6; i++) {
+        inicializarvector(v, n, i);
+        listarvector(v, n);
+        printf(" %3d   %3d  \n", sumaSubMax1(v, n), sumaSubMax2(v, n));
     }
-
 }
 
-void test2(){
-    int n=9,v[n],i,k;
-    k = 10;
+void test2() {
+    int n = 9, v[n], i, k = 10;
 
     printf("                                      subM1  subM2 \n");
-
-    for(i=0; i < k ; i++){
-
-        aleatorio(v,n);
-        listarvector(v,n);
-        printf("%3d    %3d \n",sumaSubMax1(v,n),sumaSubMax2(v,n));
-
+    for (i = 0; i < k; i++) {
+        aleatorio(v, n);
+        listarvector(v, n);
+        printf("%3d    %3d \n", sumaSubMax1(v, n), sumaSubMax2(v, n));
     }
-    
 }
 
 double microsegundos() { /* obtiene la hora del sistema en microsegundos */
     struct timeval t;
-    if (gettimeofday(&t, NULL) < 0 )
+    
+    if (gettimeofday(&t, NULL) < 0)
         return 0.0;
     return (t.tv_usec + t.tv_sec * 1000000.0);
 }
 
 
-void TimeSubMax1(){
-    int n = 500,v[32000],i,k = 100;
-    double ta,tb,t,c1,c2,c3;
+void TimeSubMax1() {
+    int n = 500, v[32000], i, k = 100;
+    double ta, tb, t, c1, c2, c3;
 
-    while (n <= 32000)
-    {
-        aleatorio(v,n);
-        c1 = pow(n,1.8);c2 = pow(n,1.95);c3 = pow(n,2.2); 
+    while (n <= 32000) {
+        aleatorio(v, n);
+        c1 = pow(n, 1.8);
+        c2 = pow(n, 1.95);
+        c3 = pow(n, 2.2);
         ta = microsegundos();
-        sumaSubMax1(v,n);
+        sumaSubMax1(v, n);
         tb = microsegundos();
-        t = tb-ta;
-        if( (t)< 500 ){
+        t = tb - ta;
+        if ((t) < 500) {
             ta = microsegundos();
-            for ( i = 0; i < k; i++)
-            {
-                sumaSubMax1(v,n);
+            for (i = 0; i < k; i++) {
+                sumaSubMax1(v, n);
             }
             tb = microsegundos();
-            t = (tb-ta) / k;
-             printf("* %5d  %15f  %8f  %8f  %8f\n",n,t,t/c1,t/c2,t/c3);
-        }else{
-            printf("%5d  %15f  %8f  %8f  %8f\n",n,t,t/c1,t/c2,t/c3);
+            t = (tb - ta) / k;
+            printf("* %5d  %15f  %8f  %8f  %8f\n", n, t, t / c1, t / c2, t / c3);
+        } else {
+            printf("  %5d  %15f  %8f  %8f  %8f\n", n, t, t / c1, t / c2, t / c3);
         }
-
-        n = n *2;
+        n = n * 2;
     }
-    
 }
 
-void TimeSubMax2(){
-    int n = 500,v[32000],i,k = 10000;
-    double ta,tb,t,c1,c2,c3;
+void TimeSubMax2() {
+    int n = 500, v[32000], i, k = 10000;
+    double ta, tb, t, c1, c2, c3;
 
-    while (n <= 32000)
-    {
-        aleatorio(v,n);
-        c1 = pow(n,0.9);c2 = pow(n,1);c3 = pow(n,1.2); 
+    while (n <= 32000) {
+        aleatorio(v, n);
+        c1 = pow(n, 0.9);
+        c2 = pow(n, 1);
+        c3 = pow(n, 1.2);
         ta = microsegundos();
-        sumaSubMax2(v,n);
+        sumaSubMax2(v, n);
         tb = microsegundos();
-        t = tb-ta;
-        if( t< 500 ){
+        t = tb - ta;
+        if (t < 500) {
             ta = microsegundos();
-            for ( i = 0; i < k; i++)
-            {
-                sumaSubMax2(v,n);
+            for (i = 0; i < k; i++) {
+                sumaSubMax2(v, n);
             }
             tb = microsegundos();
-            t = (tb-ta) / k;
-            printf("* %5d  %15f  %8f  %8f  %8f\n",n,t,t/c1,t/c2,t/c3);
-        }else{
-            printf("%5d  %15f  %8f  %8f  %8f\n",n,t,t/c1,t/c2,t/c3);
+            t = (tb - ta) / k;
+            printf("* %5d  %15f  %8f  %8f  %8f\n", n, t, t / c1, t / c2, t / c3);
+        } else {
+            printf("  %5d  %15f  %8f  %8f  %8f\n", n, t, t / c1, t / c2, t / c3);
         }
-
-        n = n *2;
+        n = n * 2;
     }
-    
 }
 
-
-
-
-int main(){
-    int i = 0,n = 3;
+int main() {
+    int i = 0, n = 3;
 
     inicializar_semilla();
     printf("Test 1\n\n");
     test1();
     printf("\nTest 2\n\n");
     test2();
-    while(i < n){
-        printf("Tiempos sumasubmax1 \n");
+    while (i < n) {
+        printf("\nTiempos sumasubmax1 \n");
         TimeSubMax1();
         printf("\n");
         printf("Tiempos sumasubmax2 \n");
